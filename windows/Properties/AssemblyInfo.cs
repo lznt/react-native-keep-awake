265accffc5f8a4f4a49052f30ca12695414db50b
Lasse Annola <lasse.annola@haaveinc.com>
2017-08-08 16:37:21 +0300

    KCKeepAwake Windows Support

--- windows/Properties/AssemblyInfo.cs
+++ windows/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeviceInfo")]
+[assembly: AssemblyTitle("KCKeepAwake")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeviceInfo")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("KCKeepAwake")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
